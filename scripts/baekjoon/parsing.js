9de7b96e1778df2574e29091850eb3e23d6909a8
bye-on <codnjs842@naver.com>
2023-09-18 00:41:42 +0900

    Change: Baekjoon Convention
    내 컨벤션 형식으로 고치기

--- scripts/baekjoon/parsing.js
+++ scripts/baekjoon/parsing.js
@@ -45,29 +45,32 @@ function makeDetailMessageAndReadme(data) {
     problem_description, problem_input, problem_output,
     code, language, memory, runtime } = data;
   const score = parseNumberFromString(result);
-  const directory = `백준/${level.replace(/ .*/, '')}/${problemId}. ${convertSingleCharToDoubleChar(title)}`;
-  const message = `[${level}] Title: ${title}, Time: ${runtime} ms, Memory: ${memory} KB`
-    + ((isNaN(score)) ? ' ' : `, Score: ${score} point `) // 서브 태스크가 있는 문제로, 점수가 있는 경우 점수까지 커밋 메시지에 표기
-    + `-BaekjoonHub`;
-  const category = problem_tags.join(', ');
-  const fileName = `${convertSingleCharToDoubleChar(title)}.${languages[language]}`;
+  // const directory = `백준/${level.replace(/ .*/, '')}/${problemId}. ${convertSingleCharToDoubleChar(title)}`;
+  const directory = `dnaeuuon/BOJ`;
+  //const message = `[${level}] Title: ${title}, Time: ${runtime} ms, Memory: ${memory} KB`
+  //  + ((isNaN(score)) ? ' ' : `, Score: ${score} point `) // 서브 태스크가 있는 문제로, 점수가 있는 경우 점수까지 커밋 메시지에 표기
+  //  + `-BaekjoonHub`;
+  const message = `${problem_tags.join(', ')} ${title}`;
+  //const category = problem_tags.join(', ');
+  //const fileName = `${convertSingleCharToDoubleChar(title)}.${languages[language]}`;
+  const fileName = `${problemId}.${lang}`
   // prettier-ignore-start
-  const readme = `# [${level}] ${title} - ${problemId} \n\n`
-    + `[문제 링크](https://www.acmicpc.net/problem/${problemId}) \n\n`
-    + `### 성능 요약\n\n`
-    + `메모리: ${memory} KB, `
-    + `시간: ${runtime} ms\n\n`
-    + `### 분류\n\n`
-    + `${category || "Empty"}\n\n` + (!!problem_description ? ''
-      + `### 문제 설명\n\n${problem_description}\n\n`
-      + `### 입력 \n\n ${problem_input}\n\n`
-      + `### 출력 \n\n ${problem_output}\n\n` : '');
+  //const readme = `# [${level}] ${title} - ${problemId} \n\n`
+  //  + `[문제 링크](https://www.acmicpc.net/problem/${problemId}) \n\n`
+  //  + `### 성능 요약\n\n`
+  //  + `메모리: ${memory} KB, `
+  //  + `시간: ${runtime} ms\n\n`
+  //  + `### 분류\n\n`
+  //  + `${category || "Empty"}\n\n` + (!!problem_description ? ''
+  //    + `### 문제 설명\n\n${problem_description}\n\n`
+  //    + `### 입력 \n\n ${problem_input}\n\n`
+  //    + `### 출력 \n\n ${problem_output}\n\n` : '');
   // prettier-ignore-end
   return {
     directory,
     fileName,
     message,
-    readme,
+    readme: 'nothings',
     code
   };
 }
